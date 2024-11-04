bfc9b6ce9ba27ceaa67a47b1126729be2a240723
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-11-04 20:02:14 +0000

    Add placeholder code for handling env wrappers

--- velora/env/handlers/gym.py
+++ velora/env/handlers/gym.py
@@ -1,16 +1,32 @@
+from pathlib import Path
 from typing import Any
 import gymnasium as gym
+from pydantic import PrivateAttr
 
-from velora.conf.config import EnvConfig
+from velora.conf.config import EnvConfig, load_config
 from velora.enums import RenderMode
-from handlers import EnvHandler
+from velora.env.handlers import EnvHandler
 
 
 class GymEnvHandler(EnvHandler):
     """A Gym environment handler for working with [Gymnasium](https://gymnasium.farama.org/) environments."""
 
-    config: EnvConfig
+    config_filepath: Path | str
     env: gym.Env | None = None
+    wrappers: list[gym.Wrapper] = []
+
+    _config = PrivateAttr(None)
+    _wrapper_config = PrivateAttr(None)
+
+    @property
+    def config(self) -> EnvConfig:
+        """The environment config settings."""
+        return self._config
+
+    @property
+    def wrapper_config(self) -> GymWrapperSettings:
+        """The environment wrapper config settings."""
+        return self._wrapper_config
 
     def model_post_init(self, __context: Any) -> None:
         self.env = (
@@ -18,6 +34,29 @@ class GymEnvHandler(EnvHandler):
             if self.env is None
             else self.env
         )
+        self.config = load_config(self.config_filepath)
+
+        if self.wrappers:
+            self.env = self.__apply_wrappers()
+            self._wrapper_config = load_config(self.config_filepath, as_dict=True)[
+                "wrappers"
+            ]
+
+    def __apply_wrappers(self) -> gym.Env:
+        """Wraps one or more [gymnasium.Wrappers](https://gymnasium.farama.org/api/wrappers/table/) around a Gym environment."""
+        env = self.env
+
+        for wrapper in self.wrappers:
+            name = ""
+            if name in self.wrapper_config.keys():
+                env = wrapper(
+                    env=env,
+                    **self.wrapper_config[name],
+                )
+            else:
+                env = wrapper(env)
+
+        return env
 
     def run_demo(
         self, episodes: int = 10, render_mode: RenderMode | None = RenderMode.HUMAN
