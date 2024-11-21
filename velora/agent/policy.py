863329464976159d64beb87900bb1acd3d0843b3
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-11-21 19:46:16 +0000

    feat(policy): Added decay rate methods to `EpsilonPolicy`.

--- velora/agent/policy.py
+++ velora/agent/policy.py
@@ -3,13 +3,15 @@ from abc import ABC
 import torch
 from torch.distributions import Categorical
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict, field_validator
+
+from velora.utils.validation import device_validation
 
 
 class Policy(ABC, BaseModel):
     """A base class for agent policies."""
 
-    pass
+    model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
 class EpsilonPolicy(Policy):
@@ -17,16 +19,32 @@ class EpsilonPolicy(Policy):
     An Epsilon (ε) policy with ε-Soft and ε-Greedy approaches.
 
     Args:
-        epsilon (float, optional): Exploration probability threshold `[0, 1]` (default: 0.1)
-        device (str, optional): Device to run computations on, such as `cpu`, `cuda` (default: cpu)
+        epsilon (float, optional): Exploration probability threshold `[0, 1]` (default: 1)
+        min_epsilon (float, optional): Minimum epsilon value possible when decaying (default: 0.1)
+        decay_rate (float, optional): A fixed decay rate for epsilon. Used when calling one of the `decay` methods (default: 0.01)
+        device (str | torch.device, optional): Device to run computations on, such as `cpu`, `cuda` (default: cpu)
     """
 
-    epsilon: float = Field(0.1, ge=0, le=1)
-    device: str = "cpu"
+    epsilon: float = Field(1, ge=0, le=1)
+    min_epsilon: float = Field(0.1, ge=0, le=1)
+    decay_rate: float = 0.01
+    device: str | torch.device = Field("cpu", validate_default=True)
+
+    @field_validator("device")
+    def validate_device(cls, device: str | torch.device) -> torch.device:
+        return device_validation(device)
+
+    def decay_linear(self) -> None:
+        """Linearly decays epsilon by the decay rate."""
+        self.epsilon = max(self.min_epsilon, self.epsilon - self.decay_rate)
+
+    def decay_exp(self) -> None:
+        """Exponentially decays epsilon by the decay rate."""
+        self.epsilon = max(self.min_epsilon, self.epsilon * self.decay_rate)
 
     def greedy_action(self, actions: torch.Tensor) -> int:
         """
-        Returns an action using a using an ε-Greedy approach.
+        Returns an action using an ε-Greedy approach.
 
         Performs purely random exploration when below ε. Otherwise, selects the action with the highest value (acts greedily).
 
