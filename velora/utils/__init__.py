f90a4381ab8d67a93483d2cedba4cb69db47f145
Ryan Partridge <rpartridge101@yahoo.co.uk>
2024-12-01 12:14:50 +0000

    chore(utils): Added utility method `ignore_empty_dicts`

--- velora/utils/__init__.py
+++ velora/utils/__init__.py
@@ -1,3 +1,4 @@
+from pydantic import validate_call
 from velora.config import load_yaml, load_config
 from velora.utils.plots import plot_state_values
 
@@ -6,3 +7,9 @@ __all__ = [
     "load_config",
     "plot_state_values",
 ]
+
+
+@validate_call(validate_return=True)
+def ignore_empty_dicts(values: dict[str, dict]) -> dict:
+    """Creates a new dictionary with empty sub dictionaries removed."""
+    return {k: v for k, v in values.items() if v != {}}
