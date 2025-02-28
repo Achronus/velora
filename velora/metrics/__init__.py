201e157588c76bf0e2a35326be1d4acd1525539b
Ryan Partridge <rpartridge101@yahoo.co.uk>
2025-02-28 21:05:09 +0000

    refactor(watch): Removed `watch_notebook` method - redundant.
    Removed notebook specific method. Adds dependency bloat when notebooks are not mandatory with Velora.

--- velora/metrics/__init__.py
+++ velora/metrics/__init__.py
@@ -1,8 +1,6 @@
 from velora.metrics.tracker import MetricsTracker, TrainMetrics
-from velora.metrics.watch import watch_notebook
 
 __all__ = [
     "MetricsTracker",
     "TrainMetrics",
-    "watch_notebook",
 ]
