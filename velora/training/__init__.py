1e72f8e359456bffbd68f093dfd873c5976129ec
Ryan Partridge <rpartridge101@yahoo.co.uk>
2025-03-22 08:57:02 +0000

    test(fix): Removed pre-release code to fix test cases.

--- velora/training/__init__.py
+++ velora/training/__init__.py
@@ -1,11 +1,9 @@
 from velora.training.handler import TrainHandler
 from velora.training.metrics import MovingMetric, StepStorage, TrainMetrics
-from velora.training.vec import VecHandler
 
 __all__ = [
     "StepStorage",
     "MovingMetric",
     "TrainMetrics",
     "TrainHandler",
-    "VecHandler",
 ]
