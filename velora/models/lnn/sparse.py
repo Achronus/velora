5bd8f12e93d17f45ba064e0072970f84fe798e5b
Ryan Partridge <codemonkey37@proton.me>
2025-10-26 20:46:53 +0000

    refactor(linear): Convert `SparseLinear` from `PyTorch` -> `Jax`
    - Migrated the `SparseLinear` PyTorch module into a Flax nnx variant.
    - Added the `optax` package.

--- velora/models/lnn/sparse.py
+++ velora/models/lnn/sparse.py
@@ -1,85 +1,58 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import jax
 
-from velora.models.weight import WeightInitType, get_init_fn
+from flax import nnx
+from flax.nnx.nn import initializers
+from flax.typing import Initializer
 
 
-class SparseLinear(nn.Module):
-    """A `torch.nn.Linear` layer with sparsely weighted connections."""
+default_hidden_init = initializers.lecun_uniform()
+default_bias_init = initializers.zeros_init()
 
-    bias: torch.Tensor
-    mask: torch.Tensor
+
+class SparseLinear(nnx.Module):
+    """A linear layer with sparsely weighted connections.
+
+    Parameters:
+        in_features (int): number of input features
+        out_features (int): number of output features
+        mask (jax.Array): sparsity mask tensor of shape
+            `(out_features, in_features)`
+        rngs (flax.nnx.Rngs): random number generator key
+        hidden_init (flax.nnx.nn.initializers, optional): initializer function for the weight matrix. Default is `lecun_uniform()`
+        bias_init (flax.nnx.nn.initializers, optional): initializer function for the bias. Default is `zeros_init()`
+    """
 
     def __init__(
         self,
         in_features: int,
         out_features: int,
-        mask: torch.Tensor,
+        mask: jax.Array,
         *,
-        init_type: str | WeightInitType = "kaiming_uniform",
-        bias: bool = True,
-        device: torch.device | None = None,
+        rngs: nnx.Rngs,
+        hidden_init: Initializer = default_hidden_init,
+        bias_init: Initializer = default_bias_init,
     ) -> None:
-        """
-        Parameters:
-            in_features (int): number of input features
-            out_features (int): number of output features
-            mask (torch.Tensor): sparsity mask tensor of shape
-                `(out_features, in_features)`
-            init_type (str, optional): the type of weight initialization
-            bias (bool, optional): a flag to enable additive bias
-            device (torch.device, optional): device to perform computations on
-        """
-        super().__init__()
-
         self.in_features = in_features
         self.out_features = out_features
-        self.device = device
+        self.mask = mask
+        self.kernel_init = hidden_init
+        self.bias_init = bias_init
 
-        self.register_buffer("mask", mask.to(device).detach())
+        weight_key = rngs.params()
+        weights = hidden_init(weight_key, (in_features, out_features))
+        self.weights = nnx.Param(weights * self.mask)
 
-        weight = torch.empty((out_features, in_features), device=device)
-        self.weight = nn.Parameter(weight)
+        bias_key = rngs.params()
+        self.bias = nnx.Param(bias_init(bias_key, (out_features,)))
 
-        if bias:
-            self.bias = nn.Parameter(torch.empty(out_features, device=device))
-        else:
-            self.register_parameter("bias", None)
-
-        self.reset_parameters(init_type)
-
-        with torch.no_grad():
-            self.weight.data.mul_(self.mask)
-
-    def reset_parameters(self, style: str | WeightInitType) -> None:
+    def __call__(self, x: jax.Array) -> jax.Array:
         """
-        Initializes weights and biases using an initialization method.
-        """
-        weight_fn = get_init_fn(style)
-        weight_fn(self)
-
-    def update_mask(self, mask: torch.Tensor) -> None:
-        """
-        Updates the sparsity mask with a new one.
+        Applies a linear transformation to the inputs along the last dimension.
 
         Parameters:
-            mask (torch.Tensor): new mask
-        """
-        self.mask = mask.to(self.device).detach()
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-        Perform a forward pass through the layer.
-
-        Parameters:
-            x (torch.Tensor): input tensor with shape `(..., in_features)`
+            x (jax.Array): the array to transform with shape `(..., in_features)`
 
         Returns:
-            y_pred (torch.Tensor): layer prediction with sparsity applied with shape `(..., out_features)`.
+            y_pred (jax.Array): the layer prediction with sparsity applied. Has shape `(..., out_features)`
         """
-        return F.linear(x, self.weight * self.mask, self.bias)
-
-    def extra_repr(self) -> str:
-        """String representation of layer parameters."""
-        return f"in_features={self.in_features}, out_features={self.out_features}, bias={self.bias is not None}"
+        return x * (self.weights * self.mask) + self.bias
