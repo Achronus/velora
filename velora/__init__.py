4b10248da36699faa10aa11d3d3b407dffc95aa8
Ryan Partridge <codemonkey37@proton.me>
2026-03-26 13:20:54 +0000

    chore(init): Cleanup `__init__` files.
    Removed `_VELORA_WORKER` code logic.

--- velora/__init__.py
+++ velora/__init__.py
@@ -1,44 +1,41 @@
-import os as _os
+from velora import base, cli, disco, gym, lnn, nn, tracking, utils
+from velora.disco import (
+    DiscoAgent,
+    DiscoAgentSettings,
+    DiscoValueSettings,
+    EMASettings,
+    LossCostSettings,
+    PolicyAgentSettings,
+    RuleTrainer,
+    RuleTrainerSettings,
+)
+from velora.gym import (
+    ATARI_57,
+    ATARI_BASE,
+    ATARI_EASY,
+    ATARI_HARD,
+    ATARI_MEDIUM,
+    EnvGroup,
+    EnvSet,
+)
+from velora.tracking import CheckpointSettings, MetricLoggerSettings
 
-if not _os.environ.get("_VELORA_WORKER"):
-    from velora import base, cli, disco, gym, lnn, nn, tracking, utils
-    from velora.disco import (
-        DiscoAgent,
-        DiscoAgentSettings,
-        DiscoValueSettings,
-        EMASettings,
-        LossCostSettings,
-        PolicyAgentSettings,
-        RuleTrainer,
-        RuleTrainerSettings,
-    )
-    from velora.gym import (
-        ATARI_57,
-        ATARI_BASE,
-        ATARI_EASY,
-        ATARI_HARD,
-        ATARI_MEDIUM,
-        EnvGroup,
-        EnvSet,
-    )
-    from velora.tracking import CheckpointSettings, MetricLoggerSettings
-
-    __all__ = [
-        "RuleTrainer",
-        "RuleTrainerSettings",
-        "DiscoAgent",
-        "PolicyAgentSettings",
-        "DiscoAgentSettings",
-        "DiscoValueSettings",
-        "EMASettings",
-        "LossCostSettings",
-        "CheckpointSettings",
-        "MetricLoggerSettings",
-        "ATARI_BASE",
-        "ATARI_EASY",
-        "ATARI_MEDIUM",
-        "ATARI_HARD",
-        "ATARI_57",
-        "EnvSet",
-        "EnvGroup",
-    ]
+__all__ = [
+    "RuleTrainer",
+    "RuleTrainerSettings",
+    "DiscoAgent",
+    "PolicyAgentSettings",
+    "DiscoAgentSettings",
+    "DiscoValueSettings",
+    "EMASettings",
+    "LossCostSettings",
+    "CheckpointSettings",
+    "MetricLoggerSettings",
+    "ATARI_BASE",
+    "ATARI_EASY",
+    "ATARI_MEDIUM",
+    "ATARI_HARD",
+    "ATARI_57",
+    "EnvSet",
+    "EnvGroup",
+]
--- velora/gym/__init__.py
+++ velora/gym/__init__.py
@@ -1,32 +1,29 @@
-import os as _os
+from velora.gym.envs import (
+    ATARI_57,
+    ATARI_BASE,
+    ATARI_EASY,
+    ATARI_HARD,
+    ATARI_MEDIUM,
+    AtariEnvs,
+    EnvGroup,
+    EnvSet,
+    MakeFn,
+)
+from velora.gym.error import MissingPackageError
+from velora.gym.make import make_atari_env
+from velora.gym.wrappers import FrameStackReshape
 
-if not _os.environ.get("_VELORA_WORKER"):
-    from velora.gym.envs import (
-        ATARI_57,
-        ATARI_BASE,
-        ATARI_EASY,
-        ATARI_HARD,
-        ATARI_MEDIUM,
-        AtariEnvs,
-        EnvGroup,
-        EnvSet,
-        MakeFn,
-    )
-    from velora.gym.error import MissingPackageError
-    from velora.gym.make import make_atari_env
-    from velora.gym.wrappers import FrameStackReshape
-
-    __all__ = [
-        "ATARI_BASE",
-        "ATARI_EASY",
-        "ATARI_MEDIUM",
-        "ATARI_HARD",
-        "ATARI_57",
-        "AtariEnvs",
-        "EnvGroup",
-        "EnvSet",
-        "MakeFn",
-        "MissingPackageError",
-        "make_atari_env",
-        "FrameStackReshape",
-    ]
+__all__ = [
+    "ATARI_BASE",
+    "ATARI_EASY",
+    "ATARI_MEDIUM",
+    "ATARI_HARD",
+    "ATARI_57",
+    "AtariEnvs",
+    "EnvGroup",
+    "EnvSet",
+    "MakeFn",
+    "MissingPackageError",
+    "make_atari_env",
+    "FrameStackReshape",
+]
