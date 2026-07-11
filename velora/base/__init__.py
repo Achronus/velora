26aab70b7bdaced3941cab5283fe15946ab5b10d
Ryan Partridge <codemonkey37@proton.me>
2026-07-11 21:16:46 +0100

    refactor(disco): Remove DiscoRL from Velora.
    DiscoRL is now discontinued and not planned for future usage at this time.

--- velora/base/__init__.py
+++
@@ -1,8 +0,0 @@
-from velora.base.outputs import ParamCount
-from velora.base.spec import HeadSpec, LayerSpec
-
-__all__ = [
-    "ParamCount",
-    "LayerSpec",
-    "HeadSpec",
-]
--- velora/disco/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from velora.disco.utils.compute import compute_value_outputs
-from velora.disco.utils.loss import compute_meta_reg_loss, compute_policy_loss
-
-__all__ = [
-    "compute_value_outputs",
-    "compute_meta_reg_loss",
-    "compute_policy_loss",
-]
